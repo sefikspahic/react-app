14a574650025f571f584480ad60120150a07a849
sefikspahice <spahicsefik@gmail.com>
2021-05-01 01:00:36 +0200

    Its done

---
+++ react-app/src/components/Students.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+import { list } from '../services/apiService';
+import {Link } from 'react-router-dom';
+const Students = () => {
+
+    const [students, setStudents] = useState([]);
+
+    useEffect(() => {
+        list('students', data => {
+            setStudents(data);
+        })
+    }, []);
+
+    return (
+        <div className='container'>
+        <h1>Students</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>First Name</th>
+                    <th>last Name</th>
+                    <th>Year Of Birth</th>
+                    <td> <Link to='/students/0'>Add new</Link> </td>
+                </tr>
+            </thead>
+            <tbody>
+                {students.map(c => (
+                <tr key={c._id}>
+                    <td>{c.firstName}</td>
+                    <td>{c.lastName}</td>
+                    <td>{c.yearOfBirth}</td>
+                    <td><Link to={`/students/${c._id}`}>Edit</Link></td>
+                </tr>
+                 ))}
+            </tbody>
+        </table>
+        </div>
+    );
+}
+export default Students;
